ead6d02c9eb6f762cb512d6aa31f2092d7aa327e
xander hayhoe <xanderhayhoe@gmail.com>
2023-09-19 14:43:56 -0400

    attempting valgrind integration in testing pls help

--- project_1/test.py
+++ project_1/test.py
@@ -1,27 +1,34 @@
 import subprocess
 import os
-import platform
+
 def run_test(test_number):
     input_file = os.path.join("in", f"test{test_number:02d}.in")
     output_file = os.path.join("out", f"test{test_number:02d}.out")
     print(input_file, output_file)
 
     try:
-        if platform.system() == 'Windows': 
-            result = result = subprocess.run(["a.exe"], stdin=open(input_file, "r"), stdout=subprocess.PIPE, text=True, check=True)
-        else:
-            result = subprocess.run(["./a.out"], stdin=open(input_file, "r"), stdout=subprocess.PIPE, text=True, check=True)
+        # Run your program with Valgrind for memory analysis
+        #valgrind_cmd = ["valgrind", "./a.out"]
+        #print(f"./" + {input_file})
+        print(f"valgrind ./a.out < ./{input_file}")
+        result = subprocess.run([f"valgrind ./a.out < ./{input_file}"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
+
         expected_output = open(output_file, "r").read()
-        if result.stdout == expected_output:
-            print(f"passed test{test_number:02d}")
+
+        if "ERROR SUMMARY: 0 errors" in result.stderr:
+            if result.stdout == expected_output:
+                print(f"passed test{test_number:02d}")
+            else:
+                print(f"failed test{test_number:02d}: Output mismatch")
         else:
-            print(f"failed test{test_number:02d}: Output mismatch")
+            print(f"failed test{test_number:02d}: Memory leak detected")
     except subprocess.CalledProcessError as e:
         print(f"failed test{test_number:02d}: Execution error - {e}")
     except FileNotFoundError:
         print(f"failed test{test_number:02d}: Required files not found")
     except Exception as e:
         print(f"failed test{test_number:02d}: An error occurred - {e}")
+
 num_test_cases = int(input("Please enter the number of tests you will be running: "))
 
 for test_number in range(1, num_test_cases + 1):
